e72c68cc9acf47b2b64265e0b19a4b8cbeed6210
Ronald Pico <rpico-2023564@kinal.edu.gt>
2026-09-17 12:04:11 -0600

    feat(model): agrege y avanze la clase detalleVenta

---
+++ src/org/rp/model/DetalleVenta.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.rp.model;
+
+/**
+ *
+ * @author Ronald Pico
+ * @version 1.0.0
+ * @see org.rp.model.DetalleVenta
+ * se crea la clase Detalle venta 
+ * se declara el atributo del objeto 
+ */
+public class DetalleVenta {
+     private int idDetalleVenta;
+    private int noVenta;
+    private String isbn;
+    private int cantidad;
+    private double precio;
+   
+}
