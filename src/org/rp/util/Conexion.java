0d884942c614bc597fcd06f7c1019b37f48e0fc1
Ronald pico <rpico-2023564@kinal.edu.gt>
2026-09-18 10:24:05 -0600

    feat(util) se creo y avanzo conexion

---
+++ src/org/rp/util/Conexion.java
@@ -0,0 +1,19 @@
+
+package org.rp.util;
+
+/**
+ *
+ * @author Ronald Pico
+ * @see org.rp.system.Main
+ * @version 1.0.0
+ */
+public class Conexion {
+mport java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+}
