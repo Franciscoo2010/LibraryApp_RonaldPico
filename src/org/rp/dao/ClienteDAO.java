9fb23dcf8275952e47bf3d320c834e45609e1ffa
Ronald pico <rpico-2023564@kinal.edu.gt>
2026-09-24 11:09:08 -0600

    feature(dao)se creo la clase ClienteDAOImpl

---
+++ src/org/rp/dao/ClienteDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.rp.dao;
+
+import org.rp.model.Cliente;
+
+/**
+ *
+ * @author informatica
+ */
+public interface ClienteDAO extends Crud<Cliente, Long> {
+    
+}
