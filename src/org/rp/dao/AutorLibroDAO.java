2d964cc679ad067f69e2a190be5ada0ea724caeb
Ronald pico <rpico-2023564@kinal.edu.gt>
2026-09-24 10:23:24 -0600

    feature(dao)se creo la clase AutorDAO

---
+++ src/org/rp/dao/AutorLibroDAO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.rp.dao;
+
+import org.rp.model.AutorLibro;
+
+/**
+ *
+ * @author
+ */
+public interface AutorLibroDAO  extends Crud<AutorLibro, Integer>{
+    
+
+}
