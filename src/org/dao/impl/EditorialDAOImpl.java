0f3b189a68a20d7f9df0260232c70c0feaf8cd61
Ronald pico <rpico-2023564@kinal.edu.gt>
2026-09-24 12:00:13 -0600

    feature(dao)se creo la clase EditorialDAOImpl

---
+++ src/org/dao/impl/EditorialDAOImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.dao.impl;
+
+import org.rp.dao.EditorialDAO;
+
+/**
+ *
+ * @author informatica
+ */
+public class EditorialDAOImpl implements EditorialDAO {
+    
+}
